e52f5a37cb05653aedc815e7db5e0324c8a5f6c2
Vignesh L Nambiar <vignesh@nissisoftwaresystems.com>
2022-10-31 16:57:44 +0530

    Done

---
+++ Example/Pods/Target Support Files/Pods-testlibcocoa_Tests/Pods-testlibcocoa_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testlibcocoa_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_testlibcocoa_Tests
+@end
---
+++ Example/Pods/Target Support Files/testlibcocoa/testlibcocoa-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_testlibcocoa : NSObject
+@end
+@implementation PodsDummy_testlibcocoa
+@end
