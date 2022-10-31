4eb1102cc635cfb85cb6dd02b593cb9208c3a11d
Vignesh L Nambiar <vignesh@nissisoftwaresystems.com>
2022-10-31 16:15:37 +0530

    Fist Commit

--- Example/testlibcocoa/ViewController.swift
+++ Example/testlibcocoa/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import testlibcocoa
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let test = Tester()
+        test.getprint()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ testlibcocoa/Classes/Tester.swift
@@ -0,0 +1,16 @@
+//
+//  Tester.swift
+//  testlibcocoa
+//
+//  Created by Nissi201 on 31/10/22.
+//
+
+import Foundation
+
+public class Tester {
+    public init(){}
+    
+    public func getprint(){
+        print("Hello World Dhanabal")
+    }
+}
